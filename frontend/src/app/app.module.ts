c1181b67a0f6aea59bc3206f32b5a6d83d3198bd
Christopher <donarsc@outlook.fr>
2023-10-02 17:21:11 +0200

    add rooting

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { SearchComponent } from './components/partials/search/search.component';
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import { HomeComponent } from './components/pages/home/home.component';
     AppComponent,
     HeaderComponent,
     HomeComponent,
+    SearchComponent,
     
   ],
   imports: [
